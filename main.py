df01bbf796c52f77c756f805016d5310b97506a1
FredaWerdiger <fwerdiger@gmail.com>
2022-03-16 09:46:41 +1100

    still figuring it out

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ import torch
 print_config()
 
 directory = '/media/mbcneuro/HDD1/DWI_Training_Data_INSP/'
+directory = 'D:/ctp_project_data/DWI_Training_Data_INSP/'
 root_dir = tempfile.mkdtemp() if directory is None else directory
 print(root_dir)
 
@@ -67,36 +68,13 @@ set_determinism(seed=42)
 train_transforms = Compose(
     [
         # load 4 Nifti images and stack them together
-        LoadImaged(keys=["image", "mask"]),
-        EnsureChannelFirstd(keys=["image", "mask"]),
+        LoadImaged(keys=["image", "label"]),
+        EnsureChannelFirstd(keys=["image", "label"]),
         Spacingd(
-            keys=["image", "mask"],
+            keys=["image", "label"],
             pixdim=(1.0, 1.0, 1.0),
             mode=("bilinear", "nearest"),
         ),
-        ScaleIntensityRangePercentilesd(keys=["image"],
-                                        lower=1,
-                                        upper=99,
-                                        b_min=0,
-                                        b_max=1,
-                                        channel_wise=True)])
-        NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
-        Resized(keys=["image", "mask"],
-                mode=('trilinear', 'nearest'),
-                align_corners=(True, None),
-                spatial_size=[2, 128, 128, 128]),
-        RandFlipd(keys=["image", "mask"], prob=0.5, spatial_axis=0),
-        RandFlipd(keys=["image", "mask"], prob=0.5, spatial_axis=1),
-        RandFlipd(keys=["image", "mask"], prob=0.5, spatial_axis=2),
-        RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
-        RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
-        EnsureTyped(keys=["mask", "label"]),
-    ]
-)
-val_transforms = Compose(
-    [
-        LoadImaged(keys=["image", "label"]),
-        EnsureChannelFirstd(keys="image"),
         ScaleIntensityRangePercentilesd(keys=["image"],
                                         lower=1,
                                         upper=99,
@@ -104,12 +82,32 @@ val_transforms = Compose(
                                         b_max=1,
                                         channel_wise=True),
         NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
-        Spacingd(
-            keys=["image", "label"],
-            pixdim=(1.0, 1.0, 1.0),
-            mode=("bilinear", "nearest"),
-        ),
-        Resized(keys=["image", "mask"],
+        Resized(keys=["image", "label"],
+                mode=('trilinear', 'nearest'),
+                align_corners=(True, None),
+                spatial_size=[2, 128, 128, 128]),
+        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
+        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
+        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=2),
+        RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
+        RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
+        EnsureTyped(keys=["mask", "label"]),
+    ]
+)
+val_transforms = Compose(
+    [
+        LoadImaged(keys=["image", "label"],
+                   dtype=np.float32),
+        EnsureChannelFirstd(keys="image"),
+        # ScaleIntensityRangePercentilesd(keys=["image"],
+        #                                 lower=1,
+        #                                 upper=99,
+        #                                 b_min=0,
+        #                                 b_max=1,
+        #                                 channel_wise=True),
+        NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+
+        Resized(keys=["image", "label"],
                 mode=('trilinear', 'nearest'),
                 align_corners=(True, None),
                 spatial_size=[2, 128, 128, 128]),
@@ -123,7 +121,9 @@ train_ds = Dataset(
     transform=train_transforms
 )
 
+
 train_loader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=4)
+
 val_ds = Dataset(
     root_dir=root_dir,
     transform=val_transform,
