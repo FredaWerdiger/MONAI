888a899c3ace604647c1e196facf5ea322236989
FredaWerdiger <fwerdiger@gmail.com>
2022-10-04 15:50:23 +1100

    simple model parallel evaluating on master node

--- basic_use_case_parallel.py
+++ basic_use_case_parallel.py
@@ -256,9 +256,9 @@ def example(rank, world_size):
                     val_outputs_list = [post_pred(i) for i in decollate_batch(val_outputs)]
                     val_labels = [post_pred_label(i) for i in decollate_batch(val_labels)]
                     metric(y_pred=val_outputs_list, y=val_labels)
-                # mean_dice = metric.aggregate().item()
-                # metric.reset()
-                # mean_dice_list.append(mean_dice)
+                mean_dice = metric.aggregate().item()
+                metric.reset()
+                mean_dice_list.append(mean_dice)
                 # if mean_dice > best_metric:
                 #     CHECKPOINT_PATH = root_dir + 'practice/best_model.pth'
                 #     best_metric_epoch = epoch + 1
