8f3d58b68e2906cae80f3fa9f0af6336f9b54792
FredaWerdiger <fwerdiger@gmail.com>
2022-10-10 14:20:29 +1100

    updated dice metric for parallism

--- basic_use_case_parallel.py
+++ basic_use_case_parallel.py
@@ -3,6 +3,7 @@ import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 import torch.nn as nn
+from torchmetrics import Dice
 import torch.optim as optim
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data.distributed import DistributedSampler
@@ -197,7 +198,9 @@ def example(rank, world_size):
     # define loss function and optimizer
     loss_fn = nn.MSELoss()
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
-    metric = DiceMetric(include_background=False)
+    # metric = DiceMetric(include_background=False)
+    # metric to aggregate over ddt
+    metric = Dice(dist_sync_on_step=True)
     num_epochs = 20
     val_interval = 2
     num_batches = len(train_ds) / batch_size
@@ -256,20 +259,20 @@ def example(rank, world_size):
                     val_outputs_list = [post_pred(i) for i in decollate_batch(val_outputs)]
                     val_labels = [post_pred_label(i) for i in decollate_batch(val_labels)]
                     metric(y_pred=val_outputs_list, y=val_labels)
-                # mean_dice = metric.aggregate().item()
-                # metric.reset()
-                # mean_dice_list.append(mean_dice)
-                # if mean_dice > best_metric:
-                #     CHECKPOINT_PATH = root_dir + 'practice/best_model.pth'
-                #     best_metric_epoch = epoch + 1
-                #     # only save in one process
-                #     if rank == 0:
-                #         torch.save(model.state_dict(), CHECKPOINT_PATH)
-                #     best_metric = mean_dice
-                # print(
-                #     f"Mean dice at epoch {epoch + 1}: {mean_dice:.4f}"
-                #     f"\nBest mean dice: {best_metric:.4f}; at epoch {best_metric_epoch}"
-                # )
+                mean_dice = metric.compute()
+                metric.reset()
+                mean_dice_list.append(mean_dice)
+                if mean_dice > best_metric:
+                    CHECKPOINT_PATH = root_dir + 'practice/best_model.pth'
+                    best_metric_epoch = epoch + 1
+                    # only save in one process
+                    if rank == 0:
+                        torch.save(model.state_dict(), CHECKPOINT_PATH)
+                    best_metric = mean_dice
+                print(
+                    f"Mean dice at epoch {epoch + 1}: {mean_dice:.4f}"
+                    f"\nBest mean dice: {best_metric:.4f}; at epoch {best_metric_epoch}"
+                )
     # now plot the loss and the dice
     if rank == 0:
         plt.figure("Results of training", (12,6))
