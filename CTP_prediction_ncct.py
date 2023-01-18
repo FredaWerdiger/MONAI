5a596ad52beb0bec24efc7ecedaa6c3d4768f9c7
FredaWerdiger <fwerdiger@gmail.com>
2023-01-19 09:46:07 +1100

    running simple unet

--- CTP_prediction_ncct.py
+++ CTP_prediction_ncct.py
@@ -97,7 +97,7 @@ def main(notes='', atrophy=True):
     patch_size = None
     batch_size = 2
     val_interval = 2
-    out_tag = 'ctp_net_new_threshold'
+    out_tag = 'unet_simple_DiceCE'
     out_tag = out_tag + '_atrophy' if atrophy else out_tag + '_raw_ncct'
     if not os.path.exists(directory + 'out_' + out_tag):
         os.makedirs(directory + 'out_' + out_tag)
@@ -243,7 +243,7 @@ def main(notes='', atrophy=True):
     device = 'cuda'
     channels = (16, 32, 64)
 
-    model = CTPNet(img_ch=4, output_ch=2)
+    model = U_NetCT(img_ch=4, output_ch=2)
 
     # model = torch.nn.DataParallel(model)
     model.to(device)
@@ -439,7 +439,7 @@ def main(notes='', atrophy=True):
     y = dice_metric_values_train
     plt.plot(x, y, 'k', label="Dice on training data")
     plt.legend(loc="center right")
-    plt.savefig(os.path.join(directory + 'out_' + out_tag, str(max_epochs) + '_epoch_' + model_name + '_' + loss_name + '_plot_loss.png'),
+    plt.savefig(os.path.join(directory + 'out_' + out_tag, 'loss_plot_' + str(max_epochs) + '_epoch_' + model_name + '_' + loss_name + '.png'),
                 bbox_inches='tight', dpi=300, format='png')
     plt.close()
 
