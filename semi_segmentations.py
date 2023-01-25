c876648bcf09499fd2e79092f06f85163ba8406b
FredaWerdiger <fwerdiger@gmail.com>
2023-01-25 17:20:08 +1100

    getting stats from semi segmentations

---
+++ semi_segmentations.py
@@ -0,0 +1,98 @@
+import os
+import pandas as pd
+import glob
+import SimpleITK as sitk
+
+
+HOMEDIR = os.path.expanduser('~/')
+mediaflux = HOMEDIR + 'mediaflux/'
+
+if not os.path.exists(mediaflux):
+    mediaflux = 'Z:/mediaflux/'
+
+directory = mediaflux + 'data_freda/ctp_project/CTP_DL_Data/'
+semi_masks = [file for file in glob.glob(directory + 'no_seg/masks_semi/*')
+              if ('exclude' not in file and 'funny' not in file)]
+masks = [file for file in glob.glob(directory + 'no_seg/masks/*')
+         if 'exclude' not in file]
+
+subjects = ['INSP_' + sub.split('.nii')[0].split('_')[-1] for sub in semi_masks]
+
+df = pd.DataFrame(columns=["subject",
+                           "volume_gt",
+                           "volume_pred",
+                           "abs_diff",
+                           "perc_diff",
+                           "overcall",
+                           "undercall",
+                           "blood",
+                           ])
+df.subject = subjects
+
+for sub in subjects:
+    print(f"Running for {sub}")
+    semi_mask = sitk.ReadImage([file for file in semi_masks if sub in file][0])
+    spacing = semi_mask.GetSpacing()
+    voxel_ml = spacing[0] * spacing[1] * spacing[2] / 1000
+    labelStats = sitk.LabelShapeStatisticsImageFilter()
+
+    labelStats.SetBackgroundValue(0)
+    labelStats.Execute(semi_mask)
+    try:
+        size_lesion = len(labelStats.GetIndexes(1)) * voxel_ml
+    except RuntimeError:
+        size_lesion = 0.
+    df.loc[df.subject == sub, 'volume_gt'] = round(size_lesion, 1)
+
+    mask = sitk.Cast(
+        sitk.ReadImage([file for file in masks if sub in file][0]), sitk.sitkUInt8)
+    labelStats.Execute(mask)
+    try:
+        size_prediction = len(labelStats.GetIndexes(1)) * voxel_ml
+    except RuntimeError:
+        size_prediction = 0.
+    df.loc[df.subject == sub, 'volume_pred'] = round(size_prediction, 1)
+
+    diff = size_prediction - size_lesion
+    if diff < 0:
+        df.loc[df.subject == sub, 'undercall'] = 1
+        df.loc[df.subject == sub, 'overcall'] = 0
+    elif diff > 0:
+        df.loc[df.subject == sub, 'undercall'] = 0
+        df.loc[df.subject == sub, 'overcall'] = 1
+    else:
+        df.loc[df.subject == sub, 'undercall'] = 0
+        df.loc[df.subject == sub, 'overcall'] = 0
+
+    if not size_lesion == 0:
+        perc = abs(diff)/size_lesion * 100
+    else:
+        perc = 0
+
+    df.loc[df.subject == sub, 'abs_diff'] = round(abs(diff), 1)
+    df.loc[df.subject == sub, 'perc_diff'] = round(perc, 1)
+
+df.sort_values(by='subject', inplace=True)
+
+df.to_csv('../study_design/study_lists/no_seg_stats.csv', index=False)
+
+keys = ["volume_gt", "volume_pred", "abs_diff", "perc_diff"]
+
+for key in keys:
+    df[key] = df[key].apply(pd.to_numeric, errors='coerce')
+
+df_blood = pd.read_csv('../study_design/study_lists/no_seg_stats_with_blood.csv')
+
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = sns.scatterplot(
+    x=df_blood.subject.to_list(),
+    y=df_blood.abs_diff.to_list(),
+    hue=df_blood.blood.to_list(),
+    hue_order=["no blood", "any blood"])
+ax.set(ylim=(0, 100))
+plt.show()
+
