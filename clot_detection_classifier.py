55b263bb8cd6c9a00681497fcdb35366b6d7a16d
FredaWerdiger <fwerdiger@gmail.com>
2023-03-09 11:34:20 +1100

    bigger batch size

--- clot_detection_classifier.py
+++ clot_detection_classifier.py
@@ -176,7 +176,7 @@ val_transforms = Compose(
 y_pred_trans = Compose([Activations(softmax=True)])
 y_trans = Compose([AsDiscrete(to_onehot=2)])
 
-batch_size = 2
+batch_size = 16
 train_ds = CodecDataset(train_x, train_y, train_transforms)
 train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
 
@@ -194,7 +194,7 @@ model = model.to(device)
 weights = [1, 1]
 class_weights = torch.FloatTensor(weights).to(device)
 loss_function = torch.nn.CrossEntropyLoss(weight=class_weights)
-optimizer = torch.optim.Adam(model.parameters(), 1e-5)
+optimizer = torch.optim.Adam(model.parameters(), 1e-3, weight_decay=1e-4)
 max_epochs = 200
 
 val_interval = 1
