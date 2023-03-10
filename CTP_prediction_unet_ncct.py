f5adad5050f149f30177f11c5e0fb0349322182b
FredaWerdiger <fwerdiger@gmail.com>
2023-03-10 17:28:17 +1100

    ctp preidciont unet with stratified data incl testing

--- CTP_prediction_unet_ncct.py
+++ CTP_prediction_unet_ncct.py
@@ -60,6 +60,130 @@ from torch.optim import Adam
 from models import *
 from densenet import *
 
+
+def define_zvalues(ct_img):
+    z_min = int(ct_img.shape[2] * .25)
+    z_max = int(ct_img.shape[2] * .85)
+
+    steps = int((z_max - z_min) / 18)
+
+    if steps == 0:
+        z_min = 0
+        z_max = ct_img.shape[2]
+        steps = 1
+
+    z = list(range(z_min, z_max))
+
+    rem = int(len(z) / steps) - 18
+
+    if rem < 0:
+        add_on = [z[-1] for n in range(abs(rem))]
+        z.extend(add_on)
+    elif rem % 2 == 0:
+        z_min = z_min + int(rem / 2 * steps) + 1
+        z_max = z_max - int(rem / 2 * steps) + 1
+
+    elif rem % 2 != 0:
+        z_min = z_min + math.ceil(rem / 2)
+        z_max = z_max - math.ceil(rem / 2) + 1
+
+    z = list(range(z_min, z_max, steps))
+
+    if len(z) == 19:
+        z = z[1:]
+    elif len(z) == 20:
+        z = z[1:]
+        z = z[:18]
+
+    return z
+
+
+def create_dwi_ctp_proba_map(dwi_ct_img,
+                            gt,
+                            proba_50,
+                            proba_70,
+                            proba_90,
+                            savefile,
+                            z,
+                            ext='png',
+                            save=False,
+                            dpi=250):
+    dwi_ct_img, gt, proba_50, proba_70, proba_90 = [np.rot90(im) for im in [dwi_ct_img, gt, proba_50, proba_70, proba_90]]
+    dwi_ct_img, gt, proba_50, proba_70, proba_90 = [np.fliplr(im) for im in [dwi_ct_img, gt, proba_50, proba_70, proba_90]]
+    proba_50_mask = proba_50 == 0
+    proba_70_mask = proba_70 == 0
+    proba_90_mask = proba_90 == 0
+    masked_dwi = np.ma.array(dwi_ct_img, mask=~proba_50_mask)
+    gt_mask = gt == 0
+    masked_dwi_gt = np.ma.array(dwi_ct_img, mask=~gt_mask)
+    proba_50 = np.where(proba_50 == 0, np.nan, proba_50)
+    proba_70 = np.where(proba_70 == 0, np.nan, proba_70)
+    proba_90 = np.where(proba_90 == 0, np.nan, proba_90)
+    gt = np.where(gt == 0, np.nan, gt)
+
+
+    fig, axs = plt.subplots(6, 6, facecolor='k')
+    fig.subplots_adjust(hspace=-0.1, wspace=-0.3)
+    axs = axs.ravel()
+    for ax in axs:
+        ax.axis("off")
+    for i in range(6):
+        print(i)
+
+        axs[i].imshow(dwi_ct_img[:, :, z[i]], cmap='gray',
+                      interpolation='hanning', vmin=10, vmax=dwi_ct_img.max())
+        axs[i].imshow(gt[:, :, z[i]], cmap='Reds', interpolation='hanning', alpha=0.5, vmin=0, vmax=1)
+        axs[i+6].imshow(dwi_ct_img[:, :, z[i]], cmap='gray',
+                      interpolation='hanning', vmin=10, vmax=dwi_ct_img.max())
+        axs[i+6].imshow(proba_50[:, :, z[i]], cmap='gnuplot',
+                      interpolation='hanning', alpha=1, vmin=0, vmax=1)
+        axs[i+6].imshow(proba_70[:, :, z[i]], cmap='Wistia',
+                      interpolation='hanning', alpha=1, vmin=0, vmax=1)
+        axs[i+6].imshow(proba_90[:, :, z[i]], cmap='bwr',
+                      interpolation='hanning', alpha=1, vmin=0, vmax=1)
+
+    if 12 > len(z):
+        max2 = len(z)
+    else:
+        max2 = 12
+    for i in range(6, max2):
+        print(i)
+        axs[i + 6].imshow(dwi_ct_img[:, :, z[i]], cmap='gray',
+                      interpolation='hanning', vmin=10, vmax=dwi_ct_img.max())
+        axs[i + 6].imshow(gt[:, :, z[i]], cmap='Reds', interpolation='hanning', alpha=0.5, vmin=0, vmax=1)
+        axs[i+12].imshow(dwi_ct_img[:, :, z[i]], cmap='gray',
+                      interpolation='hanning', vmin=10, vmax=dwi_ct_img.max())
+        im = axs[i+12].imshow(proba_50[:, :, z[i]], cmap='gnuplot',
+                      interpolation='hanning', alpha=1, vmin=0, vmax=1)
+        axs[i+12].imshow(proba_70[:, :, z[i]], cmap='Wistia',
+                      interpolation='hanning', alpha=1, vmin=0, vmax=1)
+        axs[i+12].imshow(proba_90[:, :, z[i]], cmap='bwr',
+                      interpolation='hanning', alpha=1, vmin=0, vmax=1)
+    if not 12 > len(z):
+        if len(z) > 18:
+            max3 = 18
+        else:
+            max3 = len(z)
+        for i in range(12, max3):
+            print(i)
+            axs[i + 12].imshow(dwi_ct_img[:, :, z[i]], cmap='gray',
+                              interpolation='hanning', vmin=10, vmax=dwi_ct_img.max())
+            axs[i + 12].imshow(gt[:, :, z[i]], cmap='Reds', interpolation='hanning', alpha=0.5, vmin=0, vmax=1)
+            axs[i + 18].imshow(dwi_ct_img[:, :, z[i]], cmap='gray',
+                               interpolation='hanning', vmin=10, vmax=dwi_ct_img.max())
+            im = axs[i + 18].imshow(proba_50[:, :, z[i]], cmap='gnuplot',
+                                    interpolation='hanning', alpha=1, vmin=0, vmax=1)
+            axs[i + 18].imshow(proba_70[:, :, z[i]], cmap='Wistia',
+                               interpolation='hanning', alpha=1, vmin=0, vmax=1)
+            axs[i + 18].imshow(proba_90[:, :, z[i]], cmap='bwr',
+                               interpolation='hanning', alpha=1, vmin=0, vmax=1)
+
+    if savefile:
+        plt.savefig(savefile, facecolor=fig.get_facecolor(), bbox_inches='tight', dpi=dpi, format=ext)
+        plt.close()
+
+
+
 def main(notes=''):
     HOMEDIR = os.path.expanduser('~/')
     if os.path.exists(HOMEDIR + 'mediaflux/'):
@@ -82,12 +206,6 @@ def main(notes=''):
         directory = 'D:/ctp_project_data/CTP_DL_Data/'
         ctp_dl_df = pd.read_csv(HOMEDIR + 'PycharmProjects/study_design/study_lists/data_for_ctp_dl.csv',
                                 usecols=['subject', 'segmentation_type', 'dl_id'])
-    # HOME MANY TRAINING FILES ARE MANUALLY SEGMENTED
-    train_df = ctp_dl_df[ctp_dl_df.apply(lambda x: 'train' in x.dl_id, axis=1)]
-    num_semi_train = len(train_df[train_df.apply(lambda x: x.segmentation_type == "semi_automated", axis=1)])
-
-    val_df = ctp_dl_df[ctp_dl_df.apply(lambda x: 'val' in x.dl_id, axis=1)]
-    num_semi_val = len(val_df[val_df.apply(lambda x: x.segmentation_type == "semi_automated", axis=1)])
 
     # train/validation/test split
 
@@ -96,6 +214,9 @@ def main(notes=''):
     image_paths.sort()
     mask_paths = glob.glob(os.path.join(data_dir, 'masks', '*'))
     mask_paths.sort()
+    ncct_paths = glob.glob(os.path.join(data_dir, 'ncct', '*'))
+    ncct_paths.sort()
+
     random_state = 42
     # create column with size of lesion (in voxels)
     lesion_size = []
@@ -108,8 +229,9 @@ def main(notes=''):
         size = label.GetNumberOfPixels(1)
         lesion_size.append(voxel_size * size)
 
+    # lesions less than 5 mL
     labels = (np.asarray(lesion_size) < 5) * 1
-    ctp_dl_df['size_labels'] = labels
+    ctp_dl_df['size_labels'] = labels # i think this is correct. maybe check
 
     num_train = int(np.ceil(0.6 * len(labels)))
     num_validation = int(np.ceil(0.2 * len(labels)))
@@ -122,8 +244,10 @@ def main(notes=''):
                                          shuffle=True,
                                          stratify=labels)
 
-    # get labels list that correspond with
-    test_labels = ctp_dl_df[ctp_dl_df.apply(lambda x: x['dl_id'] in test_id, axis=1)].size_labels.to_list()
+    # get labels list that correspond with test id
+    test_df = ctp_dl_df[ctp_dl_df.apply(lambda x: x['dl_id'] in test_id, axis=1)]
+    test_labels = test_df.size_labels.to_list()
+    test_id = test_df.dl_id.to_list()
 
     validation_id, test_id = train_test_split(test_id,
                                               train_size=num_validation,
@@ -132,21 +256,33 @@ def main(notes=''):
                                               shuffle=True,
                                               stratify=test_labels)
 
+    # HOME MANY TRAINING FILES ARE MANUALLY SEGMENTED
+    train_df = ctp_dl_df[ctp_dl_df.apply(lambda x: x.dl_id in train_id, axis=1)]
+    num_semi_train = len(train_df[train_df.apply(lambda x: x.segmentation_type == "semi_automated", axis=1)])
+
+    val_df = ctp_dl_df[ctp_dl_df.apply(lambda x: x.dl_id in validation_id, axis=1)]
+    num_semi_val = len(val_df[val_df.apply(lambda x: x.segmentation_type == "semi_automated", axis=1)])
+
+    test_df = ctp_dl_df[ctp_dl_df.apply(lambda x: x.dl_id in test_id, axis=1)]
+    num_semi_test = len(test_df[test_df.apply(lambda x: x.segmentation_type == "semi_automated", axis=1)])
+
     def make_dict(id):
         id = [str(num).zfill(3) for num in id]
         paths1 = [file for file in image_paths
                              if file.split('.nii.gz')[0].split('_')[-1] in id]
-        paths2 = [file for file in mask_paths
+        paths2 = [file for file in ncct_paths
+                  if file.split('.nii.gz')[0].split('_')[-1] in id]
+        paths3 = [file for file in mask_paths
                             if file.split('.nii.gz')[0].split('_')[-1] in id]
-        files_dict = [{"image": image_name, "label": label_name}
-                       for image_name, label_name in zip(paths1, paths2)]
+
+        files_dict = [{"image": image_name, "ncct": ncct_name, "label": label_name}
+                       for image_name, ncct_name, label_name in zip(paths1, paths2, paths3)]
 
         return files_dict
 
     train_files = make_dict(train_id)
     val_files = make_dict(validation_id)
     test_files = make_dict(test_id)
-    #TODO: Add testing piece to end of code
 
     # model parameters
     max_epochs = 400
@@ -163,7 +299,7 @@ def main(notes=''):
     patch_size = None
     batch_size = 2
     val_interval = 2
-    out_tag = 'best_model/densenet56'
+    out_tag = 'best_model/stratify_size'
     HU = 15
     atrophy = True
     # if atrophy:
@@ -177,8 +313,6 @@ def main(notes=''):
 
     set_determinism(seed=42)
 
-    train_files = BuildDataset(directory, 'train').ncct_dict[:1]
-    val_files = BuildDataset(directory, 'validation').ncct_dict[:1]
 
     transform_dir = os.path.join(directory, 'train', 'ncct_trans')
     if not os.path.exists(transform_dir):
@@ -239,6 +373,23 @@ def main(notes=''):
         ]
     )
 
+    test_transforms = Compose(
+        [
+            LoadImaged(keys=["image", "ncct", "label"]),
+            EnsureChannelFirstd(keys=["image", "ncct", "label"]),
+            Resized(keys=["image", "ncct"],
+                    mode=['trilinear', 'trilinear'],
+                    align_corners=[True, True],
+                    spatial_size=image_size*3),
+            SplitDimd(keys="image", dim=0, keepdim=True,
+                      output_postfixes=['DT', 'CBF', 'CBV', 'MTT']),
+            *atrophy_transforms,
+            ConcatItemsd(keys=features_transform, name="image", dim=0),
+            NormalizeIntensityd(keys=["image"], nonzero=True, channel_wise=True),
+            EnsureTyped(keys=["image", "label"]),
+        ]
+    )
+
     train_dataset = CacheDataset(
         data=train_files,
         transform=train_transforms,
@@ -251,6 +402,13 @@ def main(notes=''):
         cache_rate=1.0,
         num_workers=8)
 
+    test_ds = CacheDataset(
+        data=test_files,
+        transform=test_transforms,
+        cache_rate=1.0,
+        num_workers=8
+    )
+
     train_loader = DataLoader(train_dataset,
                               batch_size=batch_size,
                               shuffle=True,
@@ -259,6 +417,10 @@ def main(notes=''):
     val_loader = DataLoader(val_dataset,
                             batch_size=batch_size,
                             pin_memory=True)
+
+    test_loader = DataLoader(test_ds,
+                             batch_size=1,
+                             pin_memory=True)
 
     # # sanity check to see everything is there
     # s = 50
@@ -299,7 +461,7 @@ def main(notes=''):
         bottleneck=True,
         bottleneck_layer=4
     )
-    # model = U_Net(ch_in, 2)
+    model = U_Net(ch_in, 2)
     # model = AttU_Net(ch_in, 2)
 
     # model = torch.nn.DataParallel(model)
@@ -339,7 +501,7 @@ def main(notes=''):
     post_pred = Compose([EnsureType(), AsDiscrete(argmax=True, to_onehot=2)])
     post_label = Compose([EnsureType(), AsDiscrete(to_onehot=2)])
     start = time.time()
-    model_name = 'best_metric_' + model._get_name() +'_' + str(max_epochs) + '_' + str(HU) + 'HU' + features_string +'.pth'
+    model_path = 'best_metric_' + model._get_name() +'_' + str(max_epochs) + '_' + str(HU) + 'HU' + features_string +'.pth'
 
     for epoch in range(max_epochs):
         print("-" * 10)
@@ -409,7 +571,7 @@ def main(notes=''):
                     best_metric = mean_dice
                     best_metric_epoch = epoch + 1
                     torch.save(model.state_dict(), os.path.join(
-                        directory, 'out_' + out_tag, model_name))
+                        directory, 'out_' + out_tag, model_path))
                     print("saved new best metric model")
 
                 print(
@@ -433,6 +595,8 @@ def main(notes=''):
         myfile.write(f'Train semi-auto segmented: {num_semi_train}\n')
         myfile.write(f'Validation dataset size: {len(val_files)}\n')
         myfile.write(f'Validation semi-auto segmented: {num_semi_val}\n')
+        myfile.write(f'Test dataset size: {len(test_files)}\n')
+        myfile.write(f'Test semi-auto segmented: {num_semi_test}\n')
         myfile.write(f'Intended number of features: {len(features)}\n')
         myfile.write(f'Actual number of features: {ch_in}\n')
         myfile.write('Features: ')
@@ -480,6 +644,132 @@ def main(notes=''):
                 bbox_inches='tight', dpi=300, format='png')
     plt.close()
 
+    # test
+
+    pred_dir = os.path.join(directory + 'out_' + out_tag, "pred")
+    if not os.path.exists(pred_dir):
+        os.makedirs(pred_dir)
+
+    png_dir = os.path.join(directory + 'out_' + out_tag, "proba_pngs")
+    if not os.path.exists(png_dir):
+        os.makedirs(png_dir)
+
+    dice_metric = DiceMetric(include_background=False, reduction="mean")
+
+    post_transforms = Compose([
+        EnsureTyped(keys=["pred", "label"]),
+        Invertd(
+            keys=["pred", "proba"],
+            transform=test_transforms,
+            orig_keys=["image", "image"],
+            meta_keys=["pred_meta_dict", "pred_meta_dict"],
+            orig_meta_keys=["image_meta_dict", "image_meta_dict"],
+            meta_key_postfix="meta_dict",
+            nearest_interp=[False, False],
+            to_tensor=[True, True],
+        ),
+        AsDiscreted(keys="label", to_onehot=2),
+        AsDiscreted(keys="pred", argmax=True, to_onehot=2),
+        SaveImaged(
+            keys="proba",
+            meta_keys="pred_meta_dict",
+            output_dir=os.path.join(pred_dir, 'prob'),
+            output_postfix="proba",
+            resample=False,
+            separate_folder=False),
+        # SaveImaged(
+        #     keys="pred",
+        #     meta_keys="pred_meta_dict",
+        #     output_dir=pred_dir,
+        #     output_postfix="seg",
+        #     resample=False,
+        #     separate_folder=False)
+    ])
+
+    loader = LoadImage(image_only=True)
+    loader_meta = LoadImage(image_only=False)
+
+    model.eval()
+
+    results = pd.DataFrame(columns=['id',
+                                    'dice',
+                                    'size',
+                                    'size_pred',
+                                    'px_x',
+                                    'px_y',
+                                    'px_z',
+                                    'size_ml',
+                                    'size_pred_ml'])
+    results['id'] = ['test_' + str(item).zfill(3) for item in range(len(test_loader))]
+
+    with torch.no_grad():
+        for i, test_data in enumerate(test_loader):
+            test_inputs = test_data["image"].to(device)
+
+            test_data["pred"] = model(test_inputs)
+
+            prob = f.softmax(test_data["pred"], dim=1)  # probability of infarct
+            test_data["proba"] = prob
+
+            test_data = [post_transforms(i) for i in decollate_batch(test_data)]
+
+            test_output, test_label, test_image, test_proba = from_engine(
+                ["pred", "label", "image", "proba"])(test_data)
+
+            a = dice_metric(y_pred=test_output, y=test_label)
+            dice_score = round(a.item(), 4)
+            print(f"Dice score for image: {dice_score:.4f}")
+
+            original_image = loader_meta(test_data[0]["image_meta_dict"]["filename_or_obj"])
+            volx, voly, volz = original_image[1]['pixdim'][1:4]  # meta data
+            pixel_vol = volx * voly * volz
+
+            ground_truth = test_label[0][1].detach().numpy()
+            prediction = (test_proba[0][1].detach().numpy() >= 0.5) * 1
+            prediction_70 = (test_proba[0][1].detach().numpy() >= 0.7) * 1
+            prediction_90 = (test_proba[0][1].detach().numpy() >= 0.9) * 1
+
+            size = ground_truth.sum()
+            size_ml = size * pixel_vol / 1000
+
+            size_pred = prediction.sum()
+            size_pred_ml = size_pred * pixel_vol / 1000
+
+            name = "test_" + os.path.basename(
+                test_data[0]["image_meta_dict"]["filename_or_obj"]).split('.nii.gz')[0].split('_')[1]
+            subject = ctp_dl_df.loc[[name], "subject"].values[0]
+            try:
+                dwi_img = glob.glob(os.path.join(directory, 'dwi_test/', subject + '*'))[0]
+                dwi_img = loader(dwi_img)
+                # spartan giving an error
+                dwi_img = dwi_img.detach().numpy()
+
+                save_loc = png_dir + '/' + subject + '_proba.png'
+                create_dwi_ctp_proba_map(dwi_img, ground_truth, prediction, prediction_70, prediction_90, save_loc,
+                                         define_zvalues(dwi_img), ext='png', save=True)
+            except IndexError:
+                print("no_dwi_image")
+
+            results.loc[results.id == name, 'size'] = size
+            results.loc[results.id == name, 'size_ml'] = size_ml
+            results.loc[results.id == name, 'size_pred'] = size_pred
+            results.loc[results.id == name, 'size_pred_ml'] = size_pred_ml
+            results.loc[results.id == name, 'px_x'] = volx
+            results.loc[results.id == name, 'px_y'] = voly
+            results.loc[results.id == name, 'px_z'] = volz
+            results.loc[results.id == name, 'dice'] = dice_score
+
+        # aggregate the final mean dice result
+        metric = dice_metric.aggregate().item()
+        # reset the status for next validation round
+        dice_metric.reset()
+    print(f"Mean dice on test set: {metric:.4f}")
+    results['mean_dice'] = metric
+    results_join = results.join(
+        ctp_dl_df[~ctp_dl_df.index.duplicated(keep='first')],
+        on='id',
+        how='left')
+    results_join.to_csv(directory + 'out_' + out_tag + '/results.csv', index=False)
 
 if __name__ == "__main__":
     # Environment variables which need to be
