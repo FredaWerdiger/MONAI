908ab099661bded9e35d001e15e378d262567dc8
FredaWerdiger <fwerdiger@gmail.com>
2023-07-11 11:28:34 +1000

    changed roc curve to probability scores

--- recursive_data.py
+++ recursive_data.py
@@ -57,24 +57,41 @@ def get_semi_dataset():
 def get_corrections():
     HOMEDIR = os.path.expanduser('~/')
 
-    mediaflux = HOMEDIR + 'mediaflux'
+    mediaflux = 'Z:/'
+    if os.path.exists(HOMEDIR + 'mediaflux/'):
+        mediaflux = HOMEDIR + 'mediaflux/'
 
-    semi_data = '/data/gpfs/projects/punim1086/ctp_project/DWI_Training_Data/no_seg'
+    if not os.path.exists(mediaflux):
 
-    masks_semi = [file for file in os.listdir(os.path.join(semi_data, 'densenet_corrections'))
-                 if ('seg' in file and 'INSP_CN230483' not in file and 'INSP_CN230496' not in file)]
-    masks_semi.sort()
+        semi_data = '/data/gpfs/projects/punim1086/ctp_project/DWI_Training_Data/no_seg'
+
+        masks_semi = [file for file in os.listdir(os.path.join(semi_data, 'densenet_corrections'))
+                     if ('seg' in file and 'INSP_CN230483' not in file and 'INSP_CN230496' not in file)]
+        masks_semi.sort()
 
 
-    subjects_semi = ['INSP_' + file.split('_')[2] for file in masks_semi]
-    # for subject, file in zip(subjects_semi, masks_semi):
-    #     os.rename(os.path.join(semi_data, 'mask_semi', file),
-    #               os.path.join(semi_data, 'mask_semi', 'mask_' + subject + '.nii.gz'))
+        subjects_semi = ['INSP_' + file.split('_')[2] for file in masks_semi]
+        # for subject, file in zip(subjects_semi, masks_semi):
+        #     os.rename(os.path.join(semi_data, 'mask_semi', file),
+        #               os.path.join(semi_data, 'mask_semi', 'mask_' + subject + '.nii.gz'))
 
-    images_semi = [os.path.join(semi_data, 'densenet_corrections', 'image_' + subject + '.nii.gz')
-                   for subject in subjects_semi]
-    masks_semi = [os.path.join(semi_data, 'densenet_corrections', file) for file in masks_semi]
+        images_semi = [os.path.join(semi_data, 'densenet_corrections', 'image_' + subject + '.nii.gz')
+                       for subject in subjects_semi]
+        masks_semi = [os.path.join(semi_data, 'densenet_corrections', file) for file in masks_semi]
+    else:
+        semi_data = mediaflux + 'data_freda/dl_seg_check/'
+        masks_semi = [file for file in os.listdir(semi_data)
+                      if ('seg' in file and 'INSP_CN230483' not in file and 'INSP_CN230496' not in file)]
+        masks_semi.sort()
 
+        subjects_semi = ['INSP_' + file.split('_')[2] for file in masks_semi]
+        # for subject, file in zip(subjects_semi, masks_semi):
+        #     os.rename(os.path.join(semi_data, 'mask_semi', file),
+        #               os.path.join(semi_data, 'mask_semi', 'mask_' + subject + '.nii.gz'))
+
+        images_semi = [os.path.join(semi_data, 'image_' + subject + '.nii.gz')
+                       for subject in subjects_semi]
+        masks_semi = [os.path.join(semi_data, file) for file in masks_semi]
     semi_dict = [
         {"image": image_name, "label": label_name}
         for image_name, label_name in zip(images_semi, masks_semi)
