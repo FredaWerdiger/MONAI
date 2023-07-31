0c7d6d37de8f3272e3756da7a543e9419b56b298
FredaWerdiger <fwerdiger@gmail.com>
2023-07-31 13:07:51 +1000

    get dwis

--- get_dwis.py
+++ get_dwis.py
@@ -6,8 +6,11 @@ import shutil
 ctp_df = pd.read_csv(
             'C:/Users/fwerdiger/PycharmProjects/study_design/study_lists/data_for_ctp_dl.csv',
             usecols=['subject', 'dl_id'])
+test_df = pd.read_csv(
+    'Z:/data_freda/ctp_project/CTP_DL_Data/out_best_model/stratify_size/att_unet_3_layers/without_atrophy/complete_occlusions/upsample/results_400_epoch_AttentionUnet_DiceCELoss__DT_CBF_ncct.csv',
+usecols=['subject', 'id'])
 
-test_df = ctp_df[ctp_df.apply(lambda x: 'test' in x.dl_id, axis=1)]
+# test_df = ctp_df[ctp_df.apply(lambda x: 'test' in x.dl_id, axis=1)]
 
 subjects = test_df.subject.to_list()
 out_dir = 'D:/ctp_project_data/CTP_DL_Data/dwi_test/'
@@ -15,5 +18,8 @@ if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
 for subject in subjects:
-    dwi_img = glob.glob('Z:/INSPIRE_database/' + subject + '/CT_baseline/CTP_baseline/transform-DWI_followup/*__Warped.nii.gz')[0]
-    shutil.copy(dwi_img, os.path.join(out_dir, subject + '_dwi.nii.gz'))
+    if not os.path.exists(os.path.join(out_dir, subject + '_dwi.nii.gz')):
+        if not subject == 'INSP_AU250244':
+            print('Running for {}'.format(subject))
+            dwi_img = glob.glob('Z:/INSPIRE_database/' + subject + '/CT_baseline/CTP_baseline/transform-DWI_followup/*__Warped.nii.gz')[0]
+            shutil.copy(dwi_img, os.path.join(out_dir, subject + '_dwi.nii.gz'))
