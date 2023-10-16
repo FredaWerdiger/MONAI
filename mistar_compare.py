0fdb9d47c7099a4981a6e3b05a300a482f03ae6c
FredaWerdiger <fwerdiger@gmail.com>
2023-10-16 13:04:32 +1100

    getting correct csv

--- mistar_compare.py
+++ mistar_compare.py
@@ -114,7 +114,7 @@ def main(out_tag):
     results_folder = os.path.join(directory, 'out_' + out_tag)
     results_csv = glob.glob(results_folder + '/results*.csv')
     results_csv.sort()
-    results_csv = results_csv[0]
+    results_csv = results_csv[1]
     results_df = pd.read_csv(results_csv)
     results_df['mistar_core'] = ''
     results_df['mistar_penumbra'] = ''
